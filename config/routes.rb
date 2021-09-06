489cb4a3415e74f8639b664625ed1c789fa1d847
yuto-kawahara <mashroom1384@gmail.com>
2021-09-06 04:49:39 +0000

    [Start]Bookers2作成

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :books, only: [:index, :create, :show, :edit, :destroy, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
+end
