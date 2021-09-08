980684b7267715a5d8f85640a9676da03b4b0ce9
yuto-kawahara <mashroom1384@gmail.com>
2021-09-08 03:14:16 +0000

    [Fix]editアクションないの記述を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,10 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
-    if current_user != @user
-      redirect_to user_path(current_user)
-    else
+    if @user == current_user
       render :edit
+    else
+      redirect_to user_path(current_user)
     end
   end
   
