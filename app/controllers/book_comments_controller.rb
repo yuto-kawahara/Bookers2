a1d676f411054e56220983185a6c18b069ad7409
yuto-kawahara <mashroom1384@gmail.com>
2021-09-09 11:31:13 +0000

    [Fix]コメント投稿機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    book_comment = current_user.book_comments.new(comment_params)
+    book_comment.book_id = book.id
+    book_comment.save
+    redirect_to book_path(book_comment.book_id)
   end
 
   def destroy
-    comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    comment.destroy
-    redirect_back(fallback_location: root_path)
+    book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    book_comment.destroy
+    redirect_to book_path(book_comment.book_id)
   end
   
   private
