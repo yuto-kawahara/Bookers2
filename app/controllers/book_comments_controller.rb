d6d9b276434ea2cbb18cce6590401ba0454685ed
yuto-kawahara <mashroom1384@gmail.com>
2021-09-11 22:18:11 +0900

    [Add]コメント投稿機能の非同期処理の追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
-    redirect_to book_path(book_comment.book_id)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    @book_comment = BookComment.new
+    # redirect_to book_path(book_comment.book_id)
   end
 
   def destroy
-    book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    book_comment.destroy
-    redirect_to book_path(book_comment.book_id)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find_by(id: params[:id], book_id: @book.id)
+    @book_comment.destroy
+    @book_comment = BookComment.new
+    # redirect_to book_path(book_comment.book_id)
   end
   
   private
