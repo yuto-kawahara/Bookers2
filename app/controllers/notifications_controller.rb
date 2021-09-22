bea57c61693bf9918024845e33a056158aef1cd3
yuto-kawahara <mashroom1384@gmail.com>
2021-09-22 23:00:51 +0900

    [Fix]通知機能

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.page(params[:page]).per(20)
     @notifications.where(checked: false).each do |notification|
-      # notification.update(checked: true)
+      notification.update_attribute(:checked, true)
     end
   end
 end
--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,18 @@
 module NotificationsHelper
+  def notification_form(notification)
+    @visitor = notification.visitor
+    @comment = nil
+    your_item = link_to 'あなたの投稿', book_path(notification), style:"font-weight: bold;"
+    @visitor_comment = notification.book_comment_id
+    #notification.actionがfollowかlikeかcommentか
+    case notification.action
+      when "follow" then
+        tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"があなたをフォローしました"
+      when "favorite" then
+        tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:book_path(notification.book), style:"font-weight: bold;")+"にいいねしました"
+      when "comment" then
+        @comment = BookComment.find_by(id: @visitor_comment)&.comment
+        tag.a(@visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:book_path(notification.book), style:"font-weight: bold;")+"にコメントしました"
+    end
+  end
 end
