21c1187cd7084e282a8d09bf48ba26b08fbe794c
yuto-kawahara <mashroom1384@gmail.com>
2021-09-23 10:22:05 +0900

    [Fix]通知機能

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.page(params[:page]).per(20)
+    @notifications = @notifications.where.not(visitor_id: current_user.id)
     @notifications.where(checked: false).each do |notification|
       notification.update_attribute(:checked, true)
     end
