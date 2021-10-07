f3a250aac84f03810136144947aa7b2885e1fa89
yuto-kawahara <mashroom1384@gmail.com>
2021-10-07 13:29:53 +0900

    chart.jsによる7日間分の投稿数のグラフ表示機能を追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -14,12 +14,33 @@ module UsersHelper
   end
   
   def day_over_day_changes(today,yesterday)
-    return number_to_percentage(yesterday / today, precision:0)
+    if yesterday != 0
+      return number_to_percentage((today / yesterday.to_f) * 100, precision:0)
+    else
+      return number_to_percentage(0, precision:0)
+    end
   end
   def week_over_week_changes(this, last)
-    return number_to_percentage(last / this, precision:0)
+    if last != 0
+      return number_to_percentage((this / last.to_f) * 100, precision:0)
+    else
+      return number_to_percentage(0, precision:0)
+    end
   end
 
-  
-
+  def book_post_7days
+    @date = []
+    @count = []
+    for num in 0..6 do
+      day = num.day.ago.all_day
+      if day == Date.today.all_day
+        d = "今日"
+      else
+        d = num.to_s + "日前"
+      end
+      result = post_count(day)
+      @date.push(d)
+      @count.push(result)
+    end
+  end
 end
